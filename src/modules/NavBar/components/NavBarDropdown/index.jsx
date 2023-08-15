c6697aef52eb989899672113ba2a24b10382c5a4
davidkhachatryan02 <dkhachatryan043@gmail.com>
2023-08-15 18:55:21 +0400

    fixes

--- src/modules/NavBar/components/NavBarDropdown/index.jsx
+++ src/modules/NavBar/components/NavBarDropdown/index.jsx
@@ -13,20 +13,18 @@ const NavBarDropdown = () => {
       <button
         onClick={toggleDropdown}
         type="button"
-        className="border  hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+        className="border hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
       >
-        <svg
+        <img
           className="w-4 h-4 mr-2"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
+          xmlns="/home/developing/Documents/Davit-Khachatryan-tasks/task-1/src/assets/SadFace.svg"
           viewBox="0 0 8 19"
-        ></svg>
+        ></img>
         Open Dropdown
       </button>
 
       {isOpen && (
-        <div className="absolute mt-20 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className=" mt-20 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
           <div className="py-1">
             <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
               View on Explorer
